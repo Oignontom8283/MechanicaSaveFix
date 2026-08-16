4075d0b1a1559a3893b94b17211e7c7dde91d87a
Oignontom8283 <98918089+Oignontom8283@users.noreply.github.com>
2026-08-16 06:03:32 +0200

    Add _files Dictionary to store savefiles in memory

--- src/VirtualFS.cs
+++ src/VirtualFS.cs
@@ -1,5 +1,8 @@
+using System;
+using System.Collections.Generic;
 
 public static class VirtualFS
 {
-
+    // A dictionary to store save files in memory.
+    private static readonly Dictionary<string, string> _files = new Dictionary<string, string>(StringComparer.OrdinalIgnoreCase);
 }
