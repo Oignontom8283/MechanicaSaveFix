1f35e18fc174ecd71c92a3e678eb8ac79a7724b7
Oignontom8283 <98918089+Oignontom8283@users.noreply.github.com>
2026-08-16 05:38:30 +0200

    Refactor plugin metadata to use constants for GUID, name, and version

--- src/Main.cs
+++ src/Main.cs
@@ -3,14 +3,19 @@ using HarmonyLib;
 using UnityEngine;
 
 
-[BepInPlugin("MechanicaSaveFix", "MechanicaSaveFix", "1.0.0")]
+[BepInPlugin(MOD_GUID, MOD_NAME, MOD_VERSION)]
 public class MechanicaSaveFix : BaseUnityPlugin
 {    
+
+    public const string MOD_GUID = "com.mechanica.savefix";
+    public const string MOD_NAME = "MechanicaSaveFix";
+    public const string MOD_VERSION = "0.1.0";
+
     void Awake()
     {
         Debug.Log("[MechanicaSaveFix] starting up...");
 
-        var harmony = new Harmony("MechanicaSaveFix");
+        var harmony = new Harmony(MOD_GUID);
         harmony.PatchAll();
     }
 }
