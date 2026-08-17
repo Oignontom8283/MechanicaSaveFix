057e21f1ef84e5b82a49b7edd641d4964cebe471
Oignontom8283 <98918089+Oignontom8283@users.noreply.github.com>
2026-08-17 02:52:24 +0200

    Remove Reset()

--- src/VirtualFS.cs
+++ src/VirtualFS.cs
@@ -2,7 +2,7 @@ using System;
 using System.IO;
 using System.Collections.Generic;
 
-public enum Mode { Idle, Capturing, Playback }
+public enum Mode { Idle, Capturing, Playback,  }
 
 public static class VirtualFS
 {
@@ -24,15 +24,6 @@ public static class VirtualFS
     /// </summary>
     private static string _root;
 
-    /// <summary>
-    /// Initializes or resets the virtual file system.
-    /// </summary>
-    /// <param name="absoluteRoot"></param>
-    private static void Reset(string absoluteRoot)
-    {
-        _files.Clear();
-        _root = Path.GetFullPath(absoluteRoot);
-    }
 
     /// <summary>
     /// Begins capturing save files.
@@ -40,7 +31,15 @@ public static class VirtualFS
     /// <param name="absoluteRoot"></param>
     public static void BeginSaveCapture(string absoluteRoot)
     {
-        Reset(absoluteRoot);
-        _mode = Mode.Capturing;
+
+    }
+
+    /// <summary>
+    /// Begins playback of previously captured save files.
+    /// </summary>
+    /// <param name="absoluteRoot"></param>
+    public static void BeginLoadPlayback(string absoluteRoot)
+    {
+
     }
 }
