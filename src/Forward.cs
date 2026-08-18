11d05f746d457dd0e92bce4df24e1ef8ef772d62
Oignontom8283 <98918089+Oignontom8283@users.noreply.github.com>
2026-08-18 22:15:36 +0200

    Add Forward class to provide an interface for VirtualFS file operations

---
+++ src/Forward.cs
@@ -0,0 +1,18 @@
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+/// <summary>
+/// Provides an interface for interacting with the <see cref="VirtualFS"/> by mirroring the methods of <see cref="System.IO"/>.
+/// </summary>
+/// <remarks>
+/// The methods in this class are not documented individually; their purpose is the same as those in <see cref="System.IO"/>.
+/// </remarks>
+public static class Forward
+{   
+    // File operations
+
+    public static bool FileExists(string absolutePath) =>
+        VirtualFS.IsExistFile(absolutePath);
+
+}
