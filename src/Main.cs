0f5a21695018e6fe07195b28f538436fbc3c3b0c
Oignontom8283 <98918089+Oignontom8283@users.noreply.github.com>
2026-08-18 09:09:37 +0200

    Bump version to 0.2.0 in Main.cs

--- src/Main.cs
+++ src/Main.cs
@@ -10,7 +10,7 @@ public class MechanicaSaveFix : BaseUnityPlugin
 
     public const string MOD_GUID = "com.mechanica.savefix";
     public const string MOD_NAME = "MechanicaSaveFix";
-    public const string MOD_VERSION = "0.1.0";
+    public const string MOD_VERSION = "0.2.0";
 
     internal static ManualLogSource Log;
     private readonly Harmony harmony = new Harmony(MOD_GUID);
