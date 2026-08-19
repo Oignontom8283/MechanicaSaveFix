8c3de169ba7b9e0899f4add143d11a9da0e881b4
Oignontom8283 <98918089+Oignontom8283@users.noreply.github.com>
2026-08-19 21:22:20 +0200

    Refactor plugin metadata to use dynamic build information and enhance log messages in Awake method

--- src/Main.cs
+++ src/Main.cs
@@ -1,16 +1,16 @@
 using BepInEx;
 using BepInEx.Logging;
 using HarmonyLib;
-using UnityEngine;
-
 
 [BepInPlugin(MOD_GUID, MOD_NAME, MOD_VERSION)]
 public class MechanicaSaveFix : BaseUnityPlugin
 {    
 
-    public const string MOD_GUID = "com.mechanica.savefix";
-    public const string MOD_NAME = "MechanicaSaveFix";
-    public const string MOD_VERSION = "0.2.0";
+    public const string MOD_GUID = "com.oignontom8283.savefix";
+    public const string MOD_NAME = nameof(MechanicaSaveFix);
+    public const string MOD_VERSION = BuildInfo.Version;
+    public const string MOD_COMMIT_HASH = BuildInfo.CommitHash;
+    public const string MOD_BUILD_DATE = BuildInfo.BuildDateUtc;
 
     internal static ManualLogSource Log;
     private readonly Harmony harmony = new Harmony(MOD_GUID);
@@ -18,7 +18,11 @@ public class MechanicaSaveFix : BaseUnityPlugin
     private void Awake()
     {
         Log = Logger; // Set the logger for this plugin
-        Log.LogInfo($"{MOD_NAME} ({MOD_GUID}) version {MOD_VERSION} is loaded!");
+        Log.LogInfo(" ");
+        Log.LogInfo($" {MOD_NAME} initialized!");
+        Log.LogInfo($"   v{MOD_VERSION} - {MOD_COMMIT_HASH[..12]}");
+        Log.LogInfo($"  Built on {MOD_BUILD_DATE}");
+        Log.LogInfo(" ");
         
         // Apply Harmony patches
         harmony.PatchAll();
