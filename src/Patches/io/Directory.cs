c68e234a548a465a6831d2cd435313cf46c286bb
Oignontom8283 <98918089+Oignontom8283@users.noreply.github.com>
2026-08-20 23:55:24 +0200

    Add patch to override Directory.EnumerateFileSystemEntries method for virtual file system support

--- src/Patches/io/Directory.cs
+++ src/Patches/io/Directory.cs
@@ -252,3 +252,24 @@ public static class Patch_Directory_EnumerateDirectories_1
         return false;
     }
 }
+
+
+[HarmonyPatch]
+public static class Patch_Directory_EnumerateFileSystemEntries_1
+{
+    static System.Reflection.MethodBase TargetMethod()
+    {
+        return AccessTools.Method(typeof(Directory), nameof(Directory.EnumerateFileSystemEntries), new[] { typeof(string) });
+    }
+
+    static bool Prefix(string path, ref IEnumerable<string> __result)
+    {
+        if (!VirtualFS.InScope(path))
+        {
+            return true;
+        }
+        
+        __result = Forward.EnumerateFileSystemEntries(path);
+        return false;
+    }
+}
